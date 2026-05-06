6e656792555ea9d970d8550bcf55e9af9bc78c83
alk096 <mahamsdoualker@gmail.com>
2026-05-06 21:18:48 +0100

    render deployement database with admin user

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ python manage.py collectstatic --no-input
 
 # Migration de la base de données
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='papy-sept').exists() or User.objects.create_superuser('papy-sept', '', '123456789')"
